10044d50e8f79aa95a5d46ee9895bfa0076c7b61
Bruce Marriner <bruce@sqls.net>
2015-11-23 09:45:07 -0600

    Initial add of discord.go to be used for higher level functions

---
+++ discord.go
@@ -0,0 +1,16 @@
+// Discordgo - Go bindings for Discord
+// Available at https://github.com/bwmarrin/discordgo
+
+// Copyright 2015 Bruce Marriner <bruce@sqls.net>.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This file contains high level helper functions and easy entry points for the
+// entire discordgo package.  These functions are beling developed and are very
+// experimental at this point.  They will most likley change so please use the
+// low level functions if that's a problem.
+
+// package discordgo provides Go bindings for Discord
+package discordgo
+
+// TODO add easy to use high level helper functions
