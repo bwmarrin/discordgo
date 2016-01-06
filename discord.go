ad7b7603c6160b22af1c3bf708faee12f3af68fa
Bruce Marriner <bruce@sqls.net>
2016-01-06 16:36:34 -0600

    Bump version for v0.8.0

--- discord.go
+++ discord.go
@@ -16,7 +16,7 @@ package discordgo
 import "fmt"
 
 // DiscordGo Version, follows Symantic Versioning. (http://semver.org/)
-const VERSION = "0.8.0-alpha"
+const VERSION = "0.8.0"
 
 /*
 type Config struct {
