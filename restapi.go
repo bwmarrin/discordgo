ae8d31f031e1bfc4e82bd35f1e56b5c8656adb43
Bela <bela.stoyan@gmail.com>
2016-09-26 17:17:47 +0200

    Fix for 401 Auth Error

--- restapi.go
+++ restapi.go
@@ -86,7 +86,7 @@ func (s *Session) request(method, urlStr, contentType string, b []byte) (respons
 	// Not used on initial login..
 	// TODO: Verify if a login, otherwise complain about no-token
 	if s.Token != "" {
-		req.Header.Set("authorization", s.Token)
+		req.Header.Set("authorization", "Bot " + s.Token)
 	}
 
 	req.Header.Set("Content-Type", contentType)
