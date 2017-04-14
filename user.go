9b9f2894154e3ee50c2165086873a0785ed4c24d
LEGOlord208 <LEGOlord208@gmail.com>
2017-04-14 18:07:36 +0200

    How about no? (#356)
    I don't know why somebody would do something this inefficient, but I think plain concatination is a better approach

--- user.go
+++ user.go
@@ -1,9 +1,5 @@
 package discordgo
 
-import (
-	"fmt"
-)
-
 // A User stores all data for an individual Discord user.
 type User struct {
 	ID            string `json:"id"`
@@ -19,5 +15,5 @@ type User struct {
 
 //String returns a unique identifier of the form username#discriminator
 func (u *User) String() string {
-	return fmt.Sprintf("%s#%s", u.Username, u.Discriminator)
+	return u.Username + "#" + u.Discriminator
 }
