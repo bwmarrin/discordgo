9a7f16cd04184a4b4136fbce59d330a8730aa729
Kevin Minter <kminttech@gmail.com>
2017-04-29 16:11:13 -0500

    Changed test format to test all operation tests of guild struct in one test

--- state_test.go
+++ state_test.go
@@ -22,7 +22,7 @@ func TestNewState(t *testing.T)  {
   }
 }
 
-func TestGuildAdd(t *testing.T) {
+func TestGuildOperations(t *testing.T) {
   s, err := NewState()
   if err != nil {
     t.Errorf("NewState() returned error: %+v", err)
@@ -30,8 +30,31 @@ func TestGuildAdd(t *testing.T) {
 
   var g Guild
 
+  g.guildID = "Test Guild"
+
   _, err := s.GuildAdd(g)
   if err != nil {
     t.Errorf("GuildAdd() returned error: %+v", err)
+  }
+
+  g2, err := s.Guild("Test Guild")
+  if err != nil {
+    t.Errorf("Guild() returned error: %+v", err)
+  }
+  if g2 == nil {
+    t.Errorf("Guild not found when added")
+  }
+
+  _, err := s.GuildRemove(g)
+  if err != nil {
+    t.Errorf("GuildRemove() returned error: %+v", err)
+  }
+
+  g3, err := s.Guild("Test Guild")
+  if err != nil {
+    t.Errorf("Guild() returned error: %+v", err)
+  }
+  if g3 == nil {
+    t.Errorf("Guild correctly not found after removal expected error")
   }
 }
