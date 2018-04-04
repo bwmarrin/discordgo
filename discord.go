6bc9907cfa5aecee4fa3b747e1df715f44f9ccb5
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2018-04-04 16:00:41 +0100

    Removed changes
    Need to learn git *properly* :(

--- discord.go
+++ discord.go
@@ -21,7 +21,7 @@ import (
 )
 
 // VERSION of DiscordGo, follows Semantic Versioning. (http://semver.org/)
-const VERSION = "0.18.0"
+const VERSION = "0.18.0-alpha"
 
 // ErrMFA will be risen by New when the user has 2FA.
 var ErrMFA = errors.New("account has 2FA enabled")
