0f0aaad0115d19936000eeb481b733672eef6b8b
Carson Hoffman <c@rsonhoffman.com>
2021-01-20 19:48:09 -0500

    Correct header in rate limit test

--- ratelimit_test.go
+++ ratelimit_test.go
@@ -57,7 +57,7 @@ func TestRatelimitGlobal(t *testing.T) {
 
 		headers.Set("X-RateLimit-Global", "1")
 		// Reset for approx 1 seconds from now
-		headers.Set("Retry-After", "1000")
+		headers.Set("X-RateLimit-Reset-After", "1")
 
 		err := bucket.Release(headers)
 		if err != nil {
