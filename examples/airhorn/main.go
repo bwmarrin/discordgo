e339ff173f1fa79f90a6db937c28b9af0733e8d8
Marcos Dionisio <marcos.m.dionisio@hotmail.com>
2021-04-27 20:31:29 -0400

    Best way to detect that the author is not a bot

--- examples/airhorn/main.go
+++ examples/airhorn/main.go
@@ -87,7 +87,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
+	if m.Author.Bot {
 		return
 	}
 
