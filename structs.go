6b49261f28a3a8a7c010a259ac62b136212f8671
Cyrus <50967051+Not-Cyrus@users.noreply.github.com>
2021-05-02 00:34:18 -0400

    fix ability to turn off slowmode via ChannelEdit

--- structs.go
+++ structs.go
@@ -315,7 +315,7 @@ type ChannelEdit struct {
 	UserLimit            int                    `json:"user_limit,omitempty"`
 	PermissionOverwrites []*PermissionOverwrite `json:"permission_overwrites,omitempty"`
 	ParentID             string                 `json:"parent_id,omitempty"`
-	RateLimitPerUser     int                    `json:"rate_limit_per_user,omitempty"`
+	RateLimitPerUser     *int                    `json:"rate_limit_per_user,omitempty"`
 }
 
 // A ChannelFollow holds data returned after following a news channel
