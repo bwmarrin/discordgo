ec2cf69202639bf17e57d9cee85f5c35659ad320
Pierce <pierce@vulpes.dev>
2021-05-14 22:18:50 -0400

    Remove deprecated response types (#930)
    * update deprecated notices
    
    * remove deprecated response types
    
    * remove InteractionResponseChannelMessage usage
    
    * remove deprecated response types from example options
    
    * stop using iota for respone type constants

--- interactions.go
+++ interactions.go
@@ -227,18 +227,11 @@ type InteractionResponseType uint8
 // Interaction response types.
 const (
 	// InteractionResponsePong is for ACK ping event.
-	InteractionResponsePong = InteractionResponseType(iota + 1)
-	// InteractionResponseAcknowledge is for ACK a command without sending a message, eating the user's input.
-	// NOTE: this type is being imminently deprecated, and **will be removed when this occurs.**
-	InteractionResponseAcknowledge
-	// InteractionResponseChannelMessage is for responding with a message, eating the user's input.
-	// NOTE: this type is being imminently deprecated, and **will be removed when this occurs.**
-	InteractionResponseChannelMessage
+	InteractionResponsePong InteractionResponseType = 1
 	// InteractionResponseChannelMessageWithSource is for responding with a message, showing the user's input.
-	InteractionResponseChannelMessageWithSource
+	InteractionResponseChannelMessageWithSource InteractionResponseType = 4
 	// InteractionResponseDeferredChannelMessageWithSource acknowledges that the event was received, and that a follow-up will come later.
-	// It was previously named InteractionResponseACKWithSource.
-	InteractionResponseDeferredChannelMessageWithSource
+	InteractionResponseDeferredChannelMessageWithSource InteractionResponseType = 5
 )
 
 // InteractionResponse represents a response for an interaction event.
