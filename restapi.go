fb0692b08d3bed76304ee3f93694e1a98b659879
Ceriath <gh@ceriath.net>
2022-04-08 00:23:20 +0200

    Add example of emojiID to MessageReactionAdd (#798)

--- restapi.go
+++ restapi.go
@@ -2310,7 +2310,7 @@ func (s *Session) WebhookMessageDelete(webhookID, token, messageID string) (err 
 // MessageReactionAdd creates an emoji reaction to a message.
 // channelID : The channel ID.
 // messageID : The message ID.
-// emojiID   : Either the unicode emoji for the reaction, or a guild emoji identifier.
+// emojiID   : Either the unicode emoji for the reaction, or a guild emoji identifier in name:id format (e.g. "hello:1234567654321")
 func (s *Session) MessageReactionAdd(channelID, messageID, emojiID string) error {
 
 	// emoji such as  #⃣ need to have # escaped
