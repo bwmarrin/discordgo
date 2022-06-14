e488ec99a893b626d7aaa1627f120c6a8fabf36b
Elias* <31409841+EliasStar@users.noreply.github.com>
2022-06-14 22:42:37 +0200

    Fix formatting

--- restapi.go
+++ restapi.go
@@ -2814,7 +2814,7 @@ func (s *Session) ApplicationCommands(appID, guildID string) (cmd []*Application
 		endpoint = EndpointApplicationGuildCommands(appID, guildID)
 	}
 
-	body, err := s.RequestWithBucketID("GET", endpoint + "?with_localizations=true", nil, endpoint)
+	body, err := s.RequestWithBucketID("GET", endpoint+"?with_localizations=true", nil, endpoint)
 	if err != nil {
 		return
 	}
