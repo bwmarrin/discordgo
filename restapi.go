7031ab43ecbca858b5bca0a5576c001134868534
Ishan Goel <igoel.mail@gmail.com>
2023-04-28 08:11:45 -0700

    Fix bug with error not being returned in WebhookEditWithToken

--- restapi.go
+++ restapi.go
@@ -2302,8 +2302,9 @@ func (s *Session) WebhookEditWithToken(webhookID, token, name, avatar string, op
 		Name   string `json:"name,omitempty"`
 		Avatar string `json:"avatar,omitempty"`
 	}{name, avatar}
-
-	body, err := s.RequestWithBucketID("PATCH", EndpointWebhookToken(webhookID, token), data, EndpointWebhookToken("", ""), options...)
+	
+	var body []byte
+	body, err = s.RequestWithBucketID("PATCH", EndpointWebhookToken(webhookID, token), data, EndpointWebhookToken("", ""), options...)
 	if err != nil {
 		return
 	}
