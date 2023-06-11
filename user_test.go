23b139bb5233d0d38ca1cf79bee35fa3305c2b7e
Muhammad Wildan Aldiansyah <aldiwild77@gmail.com>
2023-06-11 07:05:53 +0700

    [github] fix: rename variable to more clearly
    change `tt` to `tc`. the purpose is to make the variable better communicate.
    
    Co-authored-by: Fedor Lapshin <fe.lap.prog@gmail.com>

--- user_test.go
+++ user_test.go
@@ -34,10 +34,10 @@ func TestUser(t *testing.T) {
 			want: "aldiwildan",
 		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.u.String(); got != tt.want {
-				t.Errorf("User.String() = %v, want %v", got, tt.want)
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := tc.u.String(); got != tc.want {
+				t.Errorf("User.String() = %v, want %v", got, tc.want)
 			}
 		})
 	}
