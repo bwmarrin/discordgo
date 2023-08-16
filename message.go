cc13f0042c3dc716cbdd4ff0c1d27eb2d5b896ce
ozraru <ozraru@raru.work>
2023-08-16 21:20:43 +0900

    Fix type of flags field in MessageSend struct

--- message.go
+++ message.go
@@ -234,7 +234,7 @@ type MessageSend struct {
 	AllowedMentions *MessageAllowedMentions `json:"allowed_mentions,omitempty"`
 	Reference       *MessageReference       `json:"message_reference,omitempty"`
 	StickerIDs      []string                `json:"sticker_ids"`
-	Flags           int                     `json:"flags,omitempty"`
+	Flags           MessageFlags            `json:"flags,omitempty"`
 
 	// TODO: Remove this when compatibility is not required.
 	File *File `json:"-"`
