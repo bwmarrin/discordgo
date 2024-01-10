97c11780748440f5a478077e139a052c5304963e
Fedor Lapshin <fe.lap.prog@gmail.com>
2024-01-10 11:17:50 +0300

    feat(*): bump version to 0.26.3

--- discord.go
+++ discord.go
@@ -22,7 +22,7 @@ import (
 )
 
 // VERSION of DiscordGo, follows Semantic Versioning. (http://semver.org/)
-const VERSION = "0.26.2"
+const VERSION = "0.26.3"
 
 // New creates a new Discord session with provided token.
 // If the token is for a bot, it must be prefixed with "Bot "
