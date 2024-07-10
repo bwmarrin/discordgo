f51dd6533ffcba72ffe3f39c7374bb90e6fce319
Emma Alyx Wunder <hallo@emmalyx.dev>
2024-07-11 01:55:38 +0200

    Added another comment

--- events.go
+++ events.go
@@ -277,6 +277,7 @@ func (m *MessageDelete) UnmarshalJSON(b []byte) error {
 // MessageReactionType is the type of reaction. Burst-type reactions are Super Reactions.
 type MessageReactionType int
 
+// Block contains all known MessageReactionType values.
 const (
 	MessageReactionTypeNormal MessageReactionType = 0
 	MessageReactionTypeBurst  MessageReactionType = 1
