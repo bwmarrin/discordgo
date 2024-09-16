8cceace1396b10598e6b51e3451beb69d3b09752
Cyb3r-Jak3 <git@Cyberjake.xyz>
2024-09-15 23:49:02 -0400

    Add additional fields to VoiceRegion struct

--- structs.go
+++ structs.go
@@ -256,9 +256,13 @@ type IntegrationAccount struct {
 }
 
 // A VoiceRegion stores data for a specific voice region server.
+// https://discord.com/developers/docs/resources/voice#voice-region-object
 type VoiceRegion struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Optimal    bool   `json:"optimal"`
+	Deprecated bool   `json:"deprecated"`
+	Custom     bool   `json:"custom"`
 }
 
 // InviteTargetType indicates the type of target of an invite
