075537beeed58022dc73a4f1a1d68ee61026c8bb
patrick xia <patrick.xia@gmail.com>
2024-11-23 10:32:19 -0800

    restapi: retry on 500s, 503s, 504s as well as 502s
    This is a sequel to issue #227.
    
    I run a matterbridge bridge and I'm getting 500s from the Discord API
    from time to time. We should treat 500, 503, 504 the same as a 502,
    instead of just immediately giving up.
    
    Since we're adding entries to the switch statement, also clean up the
    naked 429 to be consistent.

--- restapi.go
+++ restapi.go
@@ -266,6 +266,12 @@ func (s *Session) RequestWithLockedBucket(method, urlStr, contentType string, b 
 	case http.StatusOK:
 	case http.StatusCreated:
 	case http.StatusNoContent:
+	case http.StatusInternalServerError:
+		fallthrough
+	case http.StatusServiceUnavailable:
+		fallthrough
+	case http.StatusGatewayTimeout:
+		fallthrough
 	case http.StatusBadGateway:
 		// Retry sending request if possible
 		if sequence < cfg.MaxRestRetries {
@@ -275,7 +281,7 @@ func (s *Session) RequestWithLockedBucket(method, urlStr, contentType string, b 
 		} else {
 			err = fmt.Errorf("Exceeded Max retries HTTP %s, %s", resp.Status, response)
 		}
-	case 429: // TOO MANY REQUESTS - Rate limiting
+	case http.StatusTooManyRequests:
 		rl := TooManyRequests{}
 		err = Unmarshal(response, &rl)
 		if err != nil {
