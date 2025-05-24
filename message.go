1a149f2d7601c73300e33f41e15839c33466717e
Fedor Lapshin <fe.lap.prog@gmail.com>
2025-05-24 13:36:51 +0300

    feat(MessageAttachment): add flags

--- message.go
+++ message.go
@@ -352,18 +352,27 @@ type MessageAllowedMentions struct {
 
 // A MessageAttachment stores data for message attachments.
 type MessageAttachment struct {
-	ID           string  `json:"id"`
-	URL          string  `json:"url"`
-	ProxyURL     string  `json:"proxy_url"`
-	Filename     string  `json:"filename"`
-	ContentType  string  `json:"content_type"`
-	Width        int     `json:"width"`
-	Height       int     `json:"height"`
-	Size         int     `json:"size"`
-	Ephemeral    bool    `json:"ephemeral"`
-	DurationSecs float64 `json:"duration_secs"`
-	Waveform     string  `json:"waveform"`
+	ID           string                 `json:"id"`
+	URL          string                 `json:"url"`
+	ProxyURL     string                 `json:"proxy_url"`
+	Filename     string                 `json:"filename"`
+	ContentType  string                 `json:"content_type"`
+	Width        int                    `json:"width"`
+	Height       int                    `json:"height"`
+	Size         int                    `json:"size"`
+	Ephemeral    bool                   `json:"ephemeral"`
+	DurationSecs float64                `json:"duration_secs"`
+	Waveform     string                 `json:"waveform"`
+	Flags        MessageAttachmentFlags `json:"flags"`
 }
+
+// MessageAttachmentFlags is the flags of a message attachment.
+type MessageAttachmentFlags int
+
+// Valid MessageAttachmentFlags values.
+const (
+	MessageAttachmentFlagsIsRemix MessageAttachmentFlags = 1 << 2
+)
 
 // MessageEmbedFooter is a part of a MessageEmbed struct.
 type MessageEmbedFooter struct {
